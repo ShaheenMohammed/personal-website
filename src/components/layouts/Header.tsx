5ed84232a4ade1f881d2a39a83d61159b40d3d7b
Shaheeen Mohammed <shaheen072592@hotmail.com>
2026-09-07 22:19:42 -0700

    update website

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -2,9 +2,9 @@ import { RosetteIcon } from "../../assets/icons/RosetteIcon"
 
 function Header() {
   return (
-    <header className = "mx-auto text-white">
+    <header className = "mx-auto w-full text-white">
       <section className="flex justify-center text-center xl:pt-10 2xl:pt-20">
-        <div className="flex justify-between place-items-center font-extralight pt-8 gap-4 text-xs md:gap-6 md:pt-9 md:text-sm xl:text-lg xl:pt-10 xl:gap-8 2xl:text-xl 2xl:gap-8">
+        <div className="flex justify-center place-items-center font-extralight pt-8 gap-4 text-xs md:gap-6 md:pt-9 md:text-sm xl:text-lg xl:pt-10 xl:gap-8 2xl:text-xl 2xl:gap-8">
           <RosetteIcon className="w-16 md:w-25 xl:w-40 2xl:w-50" />
           <a href="https://github.com/ShaheenMohammed/">GITHUB</a>
           <a href="https://www.instagram.com/mohammed072592/">INSTAGRAM</a>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ import photo from "../assets/images/personal-website-photo-crop.jpg"
 function Home() {
 
   return (
-<div className="min-h-screen flex flex-col">
+    <div className="min-h-dvh flex flex-col">
       <h1 className="text-white pt-5 px-2 md:px-8 md:pt-8 md:text-2xl xl:pt-10 xl:pl-10 xl:text-4xl 2xl:pl-20 2xl:pt-25 2xl:text-6xl">
         <span className="italic tracking-tight">SHAHEEN</span>
         <span className="font-bold tracking-tight">MOHAMMED</span>
